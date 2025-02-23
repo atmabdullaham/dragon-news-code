c14295032d26eb63537ca08538c476270c12573f
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-02-24 02:27:26 +0600

    Design leftNavbar and fetch and show catetgoy data

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/layout-components/LeftNavbar";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
@@ -15,7 +16,9 @@ const HomeLayout = () => {
         <Navbar></Navbar>
       </nav>
       <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-2">
-        <aside className="left col-span-3">1st aside</aside>
+        <aside className="left col-span-3">
+          <LeftNavbar></LeftNavbar>
+        </aside>
         <section className="col-span-6">Main Content</section>
         <aside className="col-span-3">2nd aside</aside>
       </main>
