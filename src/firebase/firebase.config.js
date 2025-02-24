31e3d1a13a0f941395bb0a1550042a40b72203eb
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-02-24 11:58:27 +0600

    Register user recap OnAuthStateChange

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDz4VjUERF_knzQxRIr3qhmPB2OZr3tFbU",
+  authDomain: "dragon-news-code-84956.firebaseapp.com",
+  projectId: "dragon-news-code-84956",
+  storageBucket: "dragon-news-code-84956.firebasestorage.app",
+  messagingSenderId: "622610598399",
+  appId: "1:622610598399:web:a410a089af1a8a231affca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+  const { createNewUser, user, setUser } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const name = form.get("name");
+    const photoURL = form.get("photoUrl");
+    const email = form.get("email");
+    const password = form.get("password");
+    // console.log(name, photoURL, email, password);
+    createNewUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        // console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
   return (
     <div className="min-h-[calc(100vh-100px)] flex justify-center items-center">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 rounded-none p-4">
@@ -9,19 +32,39 @@ const Register = () => {
           Register Your Account
         </h2>
         <div className="card-body">
-          <form className="fieldset">
+          <form onSubmit={handleSubmit} className="fieldset">
             {/* Name */}
             <label className="label">Name</label>
-            <input type="text" className="input" placeholder="NAME" />
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="NAME"
+            />
             {/* photo url */}
             <label className="label">Photo URL</label>
-            <input type="text" className="input" placeholder="Photo Url" />
+            <input
+              name="photoUrl"
+              type="text"
+              className="input"
+              placeholder="Photo Url"
+            />
             {/* email */}
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+            />
             {/* password */}
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+            />
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
